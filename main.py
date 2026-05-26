13a172616f98bcfd3403ac894c8178ad94eaaba1
lucas <lucasdarosparente@gmail.com>
2026-05-26 11:23:01 -0300

    py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-class Cadastro:
-    def __init__(self, email, nome_usuario, senha):
+class Usuario:
+    def __init__(self, nome_de_usuario, data_de_nascimento, email, senha):
+        self.nome_de_usuario = nome_de_usuario
+        self.data_de_nascimento = data_de_nascimento
         self.email = email
-        self.nome_usuario = nome_usuario
         self.senha = senha
-
+        
